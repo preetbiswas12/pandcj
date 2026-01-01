1ca8fd8250b9868beb707cb2d0b53279e7de3365
Preet121106 <preetbiswas2006@gmail.com>
2026-01-01 22:19:54 +0530

    update marque6

--- components/CategoriesMarquee.jsx
+++ components/CategoriesMarquee.jsx
@@ -8,14 +8,14 @@ function CategoriesMarquee() {
       <div className='min-h-[150px] grid place-content-center'>
         <Component
           delay={500}
-          baseVelocity={-3}
+          baseVelocity={-1.5}
           clasname='font-bold tracking-[-0.07em] leading-[90%]'
         >
           Timeless elegance for every moment worth celebrating—discover pieces you'll treasure forever.
         </Component>
         <Component
           delay={500}
-          baseVelocity={3}
+          baseVelocity={1.5}
           clasname='font-bold tracking-[-0.07em] leading-[90%]'
         >
           Where craftsmanship meets desire—find the jewellery that tells your story.
