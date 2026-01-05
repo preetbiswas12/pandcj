2cd862b4f93a59e2745d3c2ede53db2bebf808c6
Preet121106 <preetbiswas2006@gmail.com>
2026-01-06 00:14:24 +0530

    added gtm

--- app/layout.jsx
+++ app/layout.jsx
@@ -49,6 +49,14 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <head>
+                {/* Google Tag Manager */}
+                <script dangerouslySetInnerHTML={{__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-K5DV6MLK');`}} />
+                {/* End Google Tag Manager */}
+                
                 <link rel="icon" href={assets.pandcjewellery.src} />
                 <link rel="sitemap" type="application/xml" href="/sitemap.xml" />
                 <meta name="google-site-verification" content="YOUR_GOOGLE_VERIFICATION_CODE" />
@@ -67,6 +75,13 @@ export default function RootLayout({ children }) {
                 />
             </head>
             <body className="font-amoria antialiased">
+                {/* Google Tag Manager (noscript) */}
+                <noscript>
+                    <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K5DV6MLK"
+                    height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+                </noscript>
+                {/* End Google Tag Manager (noscript) */}
+                
                 <GoogleAnalytics GA_MEASUREMENT_ID={process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID} />
                 <ClerkProvider>
                     <StoreProvider>
