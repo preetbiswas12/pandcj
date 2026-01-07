85e0317bdf1f3ec63016b47d3a17e26f60841927
Preet121106 <preetbiswas2006@gmail.com>
2026-01-07 21:01:44 +0530

    fixed login3

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -40,9 +40,9 @@ const Navbar = () => {
             <div className="mx-6">
                 <div className="flex items-center justify-between max-w-7xl mx-auto py-4  transition-all">
 
-                    <Link href="/" className="relative text-4xl font-semibold text-slate-700 flex items-center gap-3">
+                    <Link href="/" className="relative text-4xl font-semibold flex items-center gap-3">
                         
-                        <span className="text-2xl font-semibold text-slate-800"><span className="text-yellow-600">P&C</span>Jewellery<span className="text-yellow-600 text-4xl leading-0">.</span></span>
+                        <span className={`text-2xl font-semibold ${scrolled ? 'text-white' : 'text-slate-800'}`}><span className="text-yellow-600">P&C</span>Jewellery<span className="text-yellow-600 text-4xl leading-0">.</span></span>
                     </Link>
 
                     {/* Desktop Menu */}
